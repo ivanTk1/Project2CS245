0b8f2d2d18d00a7ac5846eaed1777979d10f40aa
ivanTk1 <129102812+ivanTk1@users.noreply.github.com>
2023-11-02 12:55:06 -0700

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*; 
 public class Elevator {
 
-
+    publoc
 
 
 
@@ -12,14 +12,18 @@ public class Elevator {
 
 
     public static void main(String[] args) throws IOException{
-        FileReader reader = new FileReader("db.properties");         
-        Properties p = new Properties(); 
-        p.load(reader); 
-
+        
         Person person1 = new Person("Bob", 11, 25, true);
-        System.out.println(person1.getName() + " is going to " + person1.getDestFloor() + " from " + person1.getCurFloor());
 
-        for(int i = 0; i < )
+        int floorCount = 32;
+        double passangerApears = 0.03;
+        int numberOfElevators = 1;
+        int elevatorCapacity = 10;
+        int ticks = 500;
+
+        for(int i = 0; i < ticks; i++){
+
+        }
 
 
 
