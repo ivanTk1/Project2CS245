cf3680d9213eb1313d566856f82bdf806a153123
ivanTk1 <129102812+ivanTk1@users.noreply.github.com>
2023-11-02 13:08:01 -0700

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -2,13 +2,13 @@ import java.util.*;
 import java.io.*; 
 public class Elevator {
 
-    publoc
+    public void passangerApears() {
+        
+        
 
 
-
-
-
-
+        
+    }
 
 
     public static void main(String[] args) throws IOException{
@@ -20,6 +20,13 @@ public class Elevator {
         int numberOfElevators = 1;
         int elevatorCapacity = 10;
         int ticks = 500;
+
+        Queue<Integer>[] floors = new Queue[floorCount];
+        for (int i = 0; i < floorCount; i++) {
+            floors[i] = new LinkedList<>();
+        }
+        floors[0].offer(1);
+
 
         for(int i = 0; i < ticks; i++){
 
