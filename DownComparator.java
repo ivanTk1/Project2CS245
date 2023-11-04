aadd24a2d0619c1aa12fe4ddcd66701d8e25a8e5
ivanTk1 <129102812+ivanTk1@users.noreply.github.com>
2023-11-04 12:34:55 -0700

    Update DownComparator.java

--- DownComparator.java
+++ DownComparator.java
@@ -3,7 +3,6 @@ import java.util.Comparator;
 public class DownComparator implements Comparator<Person> {
     @Override
     public int compare(Person p1, Person p2) {
-        // Custom comparison logic for the downQueue (max-heap)
         return Integer.compare(p2.getDestFloor(), p1.getDestFloor());
     }
 }
