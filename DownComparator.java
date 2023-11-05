06a9d9a7ba5253a460a56e5d31021857c427622c
ivanTk1 <129102812+ivanTk1@users.noreply.github.com>
2023-11-04 18:34:53 -0700

    Delete DownComparator.java

--- DownComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class DownComparator implements Comparator<Person> {
-    @Override
-    public int compare(Person p1, Person p2) {
-        return Integer.compare(p2.getDestFloor(), p1.getDestFloor());
-    }
-}
