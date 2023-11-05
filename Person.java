7ef844e20a5a06cd2b339a23e9b6c2273c864e49
ivanTk1 <129102812+ivanTk1@users.noreply.github.com>
2023-11-04 18:35:01 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,4 +1,4 @@
-public class Person {
+class Person {
     private String name;
     private int curFloor;
     private int destFloor;
@@ -11,42 +11,34 @@ public class Person {
         this.waiting = waiting;
     }
 
-    // Getter for 'name'
     public String getName() {
         return name;
     }
 
-    // Setter for 'name'
     public void setName(String name) {
         this.name = name;
     }
 
-    // Getter for 'curFloor'
     public int getCurFloor() {
         return curFloor;
     }
 
-    // Setter for 'curFloor'
     public void setCurFloor(int curFloor) {
         this.curFloor = curFloor;
     }
 
-    // Getter for 'destFloor'
     public int getDestFloor() {
         return destFloor;
     }
 
-    // Setter for 'destFloor'
     public void setDestFloor(int destFloor) {
         this.destFloor = destFloor;
     }
 
-    // Getter for 'waiting'
     public boolean isWaiting() {
         return waiting;
     }
 
-    // Setter for 'waiting'
     public void setWaiting(boolean waiting) {
         this.waiting = waiting;
     }
