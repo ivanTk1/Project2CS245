0ceb53b4946f7cd99ccbddc21dee23598f6963a2
ivanTk1 <129102812+ivanTk1@users.noreply.github.com>
2023-11-04 18:35:03 -0700

    Delete UpComparator.java

--- UpComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class UpComparator implements Comparator<Person> {
-    @Override
-    public int compare(Person p1, Person p2) {
-        // Custom comparison logic for the upQueue (min-heap)
-        return Integer.compare(p1.getDestFloor(), p2.getDestFloor());
-    }
-}
