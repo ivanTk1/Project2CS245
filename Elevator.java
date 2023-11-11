9b9c87e8a19634ce4a5c4babdf8803ec98e0cb08
ivanTk1 <129102812+ivanTk1@users.noreply.github.com>
2023-11-10 17:23:18 -0800

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -1,5 +1,4 @@
 import java.util.Comparator;
-import java.util.List;
 import java.util.PriorityQueue;
 
 public class Elevator {
@@ -12,15 +11,21 @@ public class Elevator {
     private static PriorityQueue<Person> maxHeapGoingDown = new PriorityQueue<>(Comparator.comparing(Person::getCurFloor).reversed());
 
     private int passengerCount = 0;
+    private int maxCapacity; // New field for maximum capacity
 
     // Initialize goingUp and goingDown at the class level
-   
-    public Elevator(String name, int curFloor, int destFloor, boolean upOrDown) {
+
+    public Elevator(String name, int curFloor, int destFloor, boolean upOrDown, int maxCapacity) {
         this.name = name;
         this.curFloor = curFloor;
         this.destFloor = destFloor;
         this.upOrDown = upOrDown;
+        this.maxCapacity = maxCapacity;
     }
+    public void updateDirection(boolean newDirection) {
+        upOrDown = newDirection;
+    }
+
 
     public Person peekNextPersonInUpHeap() {
         if (upOrDown) {
@@ -28,55 +33,113 @@ public class Elevator {
         }
         return null; // The elevator is not going up
     }
-    
+
     public Person peekNextPersonInDownHeap() {
         if (!upOrDown) {
             return maxHeapGoingDown.peek();
         }
         return null; // The elevator is not going down
     }
-    
 
     public boolean inAction() {
         if (destFloor == curFloor && isEmpty) {
             return false;
         }
         return true;
-    }    
+    }
 
     public void addPassenger(Person passenger) {
-        if (upOrDown) {
-            minHeapGoingUp.add(passenger);
+        if (passengerCount < maxCapacity) {
+            if (passenger.getDestFloor() > passenger.getCurFloor()) {
+                minHeapGoingUp.add(passenger);
+                if (minHeapGoingUp.peek().getDestFloor() > destFloor) {
+                    destFloor = minHeapGoingUp.peek().getDestFloor();
+                }
+            } else {
+                maxHeapGoingDown.add(passenger);
+                if (maxHeapGoingDown.peek().getDestFloor() < destFloor) {
+                    destFloor = maxHeapGoingDown.peek().getDestFloor();
+                }
+            }
+            updateDirection(); // Update the direction after adding a passenger
+            isEmpty = false;
+            passengerCount++;
         } else {
-            maxHeapGoingDown.add(passenger);
+            System.out.println("Elevator is at maximum capacity. Cannot add more passengers.");
         }
-        isEmpty = false; // Elevator is not empty when passengers are added
-        passengerCount++;
     }
-    
-    
-    public Person getNextPassenger() {
+
+
+    public boolean isAtMaxCapacity() {
+        return passengerCount >= maxCapacity;
+    }
+
+    public void printPassengers() {
+        System.out.println("Passengers in " + name + ":");
+
+        if (upOrDown) {
+            System.out.println("Going Up:");
+            for (Person passenger : minHeapGoingUp) {
+                System.out.println(passenger);
+            }
+        } else {
+            System.out.println("Going Down:");
+            for (Person passenger : maxHeapGoingDown) {
+                System.out.println(passenger);
+            }
+        }
+
+        System.out.println("Total passengers: " + passengerCount);
+    }
+
+    public Person removePassenger() {
+        Person nextPassenger = null;
         if (upOrDown) {
             if (!minHeapGoingUp.isEmpty()) {
-                Person passenger = minHeapGoingUp.poll();
+                nextPassenger = minHeapGoingUp.poll();
                 if (minHeapGoingUp.isEmpty()) {
-                    isEmpty = true; // Elevator is empty when no passengers are left
+                    isEmpty = true;
                 }
-                passengerCount--;
-                return passenger;
             }
         } else {
             if (!maxHeapGoingDown.isEmpty()) {
-                Person passenger = maxHeapGoingDown.poll();
+                nextPassenger = maxHeapGoingDown.poll();
                 if (maxHeapGoingDown.isEmpty()) {
-                    isEmpty = true; // Elevator is empty when no passengers are left
+                    isEmpty = true;
                 }
-                passengerCount--;
-                return passenger;
             }
         }
-        return null; // No passenger to pick up
+    
+        if (nextPassenger != null) {
+            updateDestinationAfterRemoval();
+            passengerCount--;
+        }
+    
+        return nextPassenger;
     }
+    
+    private void updateDestinationAfterRemoval() {
+        if (upOrDown) {
+            if (!minHeapGoingUp.isEmpty()) {
+                destFloor = minHeapGoingUp.peek().getDestFloor();
+            }
+        } else {
+            if (!maxHeapGoingDown.isEmpty()) {
+                destFloor = maxHeapGoingDown.peek().getDestFloor();
+            }
+        }
+        updateDirection(); // Update the direction after removing a passenger
+    }
+    
+
+    private void updateDirection() {
+        if (!minHeapGoingUp.isEmpty() && (maxHeapGoingDown.isEmpty() || minHeapGoingUp.peek().getDestFloor() <= maxHeapGoingDown.peek().getCurFloor())) {
+            upOrDown = true; // Set direction to going up
+        } else if (!maxHeapGoingDown.isEmpty() && (minHeapGoingUp.isEmpty() || maxHeapGoingDown.peek().getDestFloor() >= minHeapGoingUp.peek().getCurFloor())) {
+            upOrDown = false; // Set direction to going down
+        }
+    }
+
     
 
     // Getters
@@ -110,7 +173,16 @@ public class Elevator {
     }
 
     public void setDestFloor(int destFloor) {
-        this.destFloor = destFloor;
+        // Check if the elevator is empty before updating the destination floor
+        if (isEmpty) {
+            this.destFloor = destFloor;
+        } else {
+            System.out.println("Cannot update destination floor while the elevator has passengers.");
+        }
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
     }
 
     public void setUpOrDown(boolean upOrDown) {
